486c8a03d97008931d69d69e10799bdd1d61a1e3
Harsh Srivastava <srivastavah240@gmail.com>
2025-05-14 00:17:31 +0530

    (feat):completed setup for clerk authenticastion , added middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
     Home Page 
+    <SignedIn>
+      <SignOutButton/>
+    </SignedIn>
+    <SignedOut>
+      <SignInButton/>
+    </SignedOut>
    </div>
   );
 }
