6dd423b61602c4e10daa5849a145bda5f1067890
Harsh Srivastava <srivastavah240@gmail.com>
2025-05-13 23:20:10 +0530

    (feat):created project basic structure , setup for shadcn

---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,13 @@
+    import React from 'react'
+
+const GenerateProgramPage
+ = () => {
+  return (
+    <div>
+        Geneate Program Page 
+    </div>
+  )
+}
+
+export default GenerateProgramPage
+
