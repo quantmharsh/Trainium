ee725ff46afeadbeeef5116a81299e9cfcec6846
Harsh Srivastava <srivastavah240@gmail.com>
2025-06-14 23:40:52 +0530

    (Feat):working on  Component

--- src/components/NoFitnessPlan.tsx
+++ src/components/NoFitnessPlan.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const NoFitnessPlan = () => {
+
+
   return (
     <div>
-      
+        No Fitness Plan Component 
     </div>
   )
 }
 
-export default NoFitnessPlan
+export default NoFitnessPlan ;
